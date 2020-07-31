d404e45320b28acabaeb7361e6f65b1107d541fb
Thiago <thi.98.cwb@hotmail.com>
2020-07-30 21:05:20 -0300

    usando casas decimais

---
+++ UsandoCasaDecimal.java
@@ -0,0 +1,12 @@
+public class UsandoCasaDecimal{
+
+	public static void main (String[] args) {
+        String nome;
+        double altura;
+
+        nome = "Goku ";
+        altura = 1.83;
+
+		System.out.println("Oi, meu nome é " + nome + "! E tenho " + altura + " metros");
+	}
+} 
